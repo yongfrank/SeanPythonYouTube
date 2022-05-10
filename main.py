9046a439dbaa00f3fa573108956edd8280743bc7
Frank Chu <yongfrank@outlook.com>
2022-05-10 21:00:48 +0800

    change feat: prompt for N

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 for i in range(5):
 	print(i)
+<<<<<<< HEAD
 for k in [0, 2, 4, 6, 8]:
 	print(k)
 
+=======
+
+N = int(input("Input N: "))
+
+for k in range(N):
+    print(k)
+>>>>>>> e436864 (feat: prompt for N)
